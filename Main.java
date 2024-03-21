c54be51b3a461fbc5f521d1382809e1e53de56a8
narges <iamsabwina@gmail.com>
2024-03-21 07:39:35 -0700

    employee class edit 1

--- Main.java
+++ Main.java
@@ -4,15 +4,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
     }
 }
-abstract class User{
+
+abstract class User {
     String fulName;
     long phoneNumber;
 
-    public User(String fulName , long phoneNumber) {
-        this.fulName=fulName; this.phoneNumber=phoneNumber;
+    public User(String fulName, long phoneNumber) {
+        this.fulName = fulName;
+        this.phoneNumber = phoneNumber;
     }
+
+    abstract String getBasicInformation();
+}
+
+abstract class Employee extends User {
+    String dateOfEmployeement;
+
+    public Employee(String fulName, long phoneNumber, String dateOfEmployeement) {
+        super(fulName, phoneNumber);
+        this.dateOfEmployeement = dateOfEmployeement;
+    }
+
+    @Override
+    final String getBasicInformation() {
+        return ("ful name: " + String.valueOf(fulName) + '\n' + "phone number: " + String.valueOf(phoneNumber));
+    }
+
+    abstract void work(int w);
+
+    abstract long getSalary();
 }
