3b9b9e718eed299b0dc24092ff83075209ba3fe1
narges <iamsabwina@gmail.com>
2024-03-21 07:00:54 -0700

    User made

--- Main.java
+++ Main.java
@@ -3,8 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// check
-        //check test
-        
+
+
+
+    }
+}
+abstract class User{
+    String fulName;
+    long phoneNumber;
+
+    public User(String fulName , long phoneNumber) {
+        this.fulName=fulName; this.phoneNumber=phoneNumber;
     }
 }
